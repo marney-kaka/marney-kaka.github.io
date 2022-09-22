0f2ca7ec2870350ea33d69ba442666cde32b72af
marney-kaka <marney.kaka@gmail.com>
2022-09-23 02:12:29 +1200

    add media queries for mobile

---
+++ blog.js
@@ -0,0 +1,13 @@
+let collapsible = document.getElementsByClassName('collapsible')
+let i
+for (i = 0; i < collapsible.length; i++) {
+  collapsible[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    let content = this.nextElementSibling
+    if (content.style.display === 'block') {
+      content.style.display = 'none'
+    } else {
+      content.style.display = 'block'
+    }
+  })
+}
